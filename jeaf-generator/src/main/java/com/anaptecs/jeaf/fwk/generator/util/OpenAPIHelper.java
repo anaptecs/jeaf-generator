604fe13ecad0b5466e2fb5e85aaf290b33c798c0
TillmannSchall <TillmannSchall@PC-TLS-003>
2021-10-13 17:46:25 +0200

    JEAF-2995 Added support for primitive types.

--- jeaf-generator/src/main/java/com/anaptecs/jeaf/fwk/generator/util/OpenAPIHelper.java
+++ jeaf-generator/src/main/java/com/anaptecs/jeaf/fwk/generator/util/OpenAPIHelper.java
@@ -1,6 +1,7 @@
 package com.anaptecs.jeaf.fwk.generator.util;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.net.URL;
 import java.util.Calendar;
 import java.util.Currency;
@@ -21,19 +22,49 @@ public class OpenAPIHelper {
   public static final Set<String> localTypes = new HashSet<String>();
 
   static {
+    // boolean
     basicTypes.put("boolean", "boolean");
     basicTypes.put("Boolean", "boolean");
     basicTypes.put(Boolean.class.getName(), "boolean");
+
+    // byte
+    basicTypes.put("byte", "integer");
+    basicTypes.put(Byte.class.getName(), "integer");
+
+    // short
+    basicTypes.put("short", "integer");
+    basicTypes.put(Short.class.getName(), "integer");
+
+    // integer
     basicTypes.put("int", "integer");
     basicTypes.put(Integer.class.getName(), "integer");
+    basicTypes.put("Integer", "integer");
+
+    // long
     basicTypes.put("long", "integer");
     basicTypes.put(Long.class.getName(), "integer");
+
+    // BigInteger
+    basicTypes.put(BigInteger.class.getName(), "integer");
+
+    // char
+    basicTypes.put("char", "string");
+    basicTypes.put(Character.class.getName(), "string");
+
+    // float
     basicTypes.put("float", "number");
     basicTypes.put(Float.class.getName(), "number");
+
+    // double
     basicTypes.put("double", "number");
     basicTypes.put(Double.class.getName(), "number");
+
+    // BigDecimal
     basicTypes.put(BigDecimal.class.getName(), "number");
+
+    // String
     basicTypes.put(String.class.getName(), "string");
+    basicTypes.put("String", "string");
 
     // We also also treat many JDK standard types as simple types in Open API as they can be mapped to a single type
     // mostly string.
@@ -48,14 +79,35 @@ public class OpenAPIHelper {
     basicTypes.put("java.time.LocalDate", "string");
     basicTypes.put("java.time.LocalDateTime", "string");
 
+    // Format mappings for byte
+    formatMapping.put("byte", "int32");
+    formatMapping.put(Byte.class.getName(), "int32");
+
+    // Format mappings for short
+    formatMapping.put("short", "int32");
+    formatMapping.put(Short.class.getName(), "int32");
+
+    // Format mappings for integer
     formatMapping.put("int", "int32");
     formatMapping.put(Integer.class.getName(), "int32");
+    formatMapping.put("Integer", "int32");
+
+    // Format mappings for long
     formatMapping.put("long", "int64");
     formatMapping.put(Long.class.getName(), "int64");
+
+    // Format mappings for BigInteger
+    formatMapping.put(BigInteger.class.getName(), "int64");
+
+    // Format mappings for float
     formatMapping.put("float", "float");
     formatMapping.put(Float.class.getName(), "float");
+
+    // Format mappings for double
     formatMapping.put("double", "double");
     formatMapping.put(Double.class.getName(), "double");
+
+    // Format mappings for BigDecimal
     formatMapping.put(BigDecimal.class.getName(), "double");
 
     formatMapping.put(java.sql.Date.class.getName(), "date-time");
