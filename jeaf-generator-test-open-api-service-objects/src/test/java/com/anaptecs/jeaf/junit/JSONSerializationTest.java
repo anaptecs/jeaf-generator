0664d8eb6964f922b75d849b5592e015b01dd9ad
tillmannschall <tillmann.schall@anaptecs.de>
2022-01-27 11:51:04 +0100

    JEAF Generator now uses JEAF JSON for serialization tests

--- jeaf-generator-test-open-api-service-objects/src/test/java/com/anaptecs/jeaf/junit/JSONSerializationTest.java
+++ jeaf-generator-test-open-api-service-objects/src/test/java/com/anaptecs/jeaf/junit/JSONSerializationTest.java
@@ -14,6 +14,8 @@ import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
 
+import com.anaptecs.jeaf.json.api.JSON;
+import com.anaptecs.jeaf.json.api.JSONTools;
 import com.anaptecs.jeaf.junit.openapi.base.Channel;
 import com.anaptecs.jeaf.junit.openapi.base.ChannelCode;
 import com.anaptecs.jeaf.junit.openapi.base.ChannelType;
@@ -22,27 +24,26 @@ import com.anaptecs.jeaf.junit.openapi.base.ChildBB;
 import com.anaptecs.jeaf.junit.openapi.base.ParentClass;
 import com.anaptecs.jeaf.junit.openapi.base.Reseller;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONSerializationTest {
 
   @Test
   public void testSimpleJSONSerialization( ) throws JsonProcessingException {
     ChannelCode lChannelCode = ChannelCode.Builder.newBuilder().setCode("POS").build();
-    ObjectMapper lObjectMapper = new ObjectMapper();
-    String lJSONString = lObjectMapper.writeValueAsString(lChannelCode);
+    JSONTools lTools = JSON.getJSONTools();
+    String lJSONString = lTools.writeObjectToString(lChannelCode);
     assertEquals("{\"code\":\"POS\"}", lJSONString);
 
-    ChannelCode lDeserializedChannelCode = lObjectMapper.readValue(lJSONString, ChannelCode.class);
+    ChannelCode lDeserializedChannelCode = lTools.readJSON(lJSONString, ChannelCode.class);
     assertEquals("POS", lDeserializedChannelCode.getCode());
 
     Channel lChannel =
         Channel.Builder.newBuilder().setChannelCode(lChannelCode).setChannelType(ChannelType.COUNTER).build();
 
-    lJSONString = lObjectMapper.writeValueAsString(lChannel);
+    lJSONString = lTools.writeObjectToString(lChannel);
     assertEquals("{\"channelType\":\"COUNTER\",\"channelCode\":{\"code\":\"POS\"}}", lJSONString);
 
-    Channel lDeserilaizedChannel = lObjectMapper.readValue(lJSONString, Channel.class);
+    Channel lDeserilaizedChannel = lTools.readJSON(lJSONString, Channel.class);
     assertEquals("POS", lDeserilaizedChannel.getChannelCode().getCode());
     assertEquals(ChannelType.COUNTER, lDeserilaizedChannel.getChannelType());
 
@@ -53,13 +54,13 @@ public class JSONSerializationTest {
     lChannels.add(lChannel);
     lChannels.add(lChannel2);
     Reseller lReseller = Reseller.Builder.newBuilder().setLanguage(Locale.GERMAN).setChannels(lChannels).build();
-    lJSONString = lObjectMapper.writeValueAsString(lReseller);
+    lJSONString = lTools.writeObjectToString(lReseller);
     assertEquals(
         "{\"channels\":[{\"channelType\":\"COUNTER\",\"channelCode\":{\"code\":\"POS\"}},{\"channelType\":\"MOBILE\",\"channelCode\":{\"code\":\"MOBILE_APP\"}}],\"products\":[],\"language\":\"de\"}",
         lJSONString);
 
     // Test deserialization
-    Reseller lDeserializedReseller = lObjectMapper.readValue(lJSONString, Reseller.class);
+    Reseller lDeserializedReseller = lTools.readJSON(lJSONString, Reseller.class);
     assertEquals(ChannelType.COUNTER, lDeserializedReseller.getChannels().get(0).getChannelType());
     assertEquals("POS", lDeserializedReseller.getChannels().get(0).getChannelCode().getCode());
     assertEquals(ChannelType.MOBILE, lDeserializedReseller.getChannels().get(1).getChannelType());
@@ -70,30 +71,30 @@ public class JSONSerializationTest {
   @Test
   public void testInheritanceSerialization( ) throws JsonProcessingException {
     ParentClass lParentClass = ParentClass.Builder.newBuilder().setParentAttribute("parent").build();
-    ObjectMapper lObjectMapper = new ObjectMapper();
-    String lValue = lObjectMapper.writeValueAsString(lParentClass);
+    JSONTools lTools = JSON.getJSONTools();
+    String lValue = lTools.writeObjectToString(lParentClass);
     assertEquals("{\"objectType\":\"ParentClass\",\"parentAttribute\":\"parent\"}", lValue);
 
-    ParentClass lDeserializedParent = lObjectMapper.readValue(lValue, ParentClass.class);
+    ParentClass lDeserializedParent = lTools.readJSON(lValue, ParentClass.class);
     assertEquals("parent", lParentClass.getParentAttribute());
 
     ChildBB lChildBB = ChildBB.Builder.newBuilder().setChildBBAttribute(123456789l).build();
-    lValue = lObjectMapper.writeValueAsString(lChildBB);
+    lValue = lTools.writeObjectToString(lChildBB);
     assertEquals(
         "{\"objectType\":\"ChildBB\",\"parentAttribute\":null,\"childBAttribute\":false,\"composition\":[],\"childBBAttribute\":123456789,\"deprecatedAttribute\":0,\"deprecatedBs\":[],\"deprecatedParent\":null,\"deprecatedArray\":null}",
         lValue);
 
-    lDeserializedParent = lObjectMapper.readValue(lValue, ParentClass.class);
+    lDeserializedParent = lTools.readJSON(lValue, ParentClass.class);
     assert (lDeserializedParent instanceof ChildBB);
 
     ChildAA lChildAA = ChildAA.Builder.newBuilder().setChildAAttribute(4711).build();
     lChildBB.addToComposition(lChildAA);
-    lValue = lObjectMapper.writeValueAsString(lChildBB);
+    lValue = lTools.writeObjectToString(lChildBB);
     assertEquals(
         "{\"objectType\":\"ChildBB\",\"parentAttribute\":null,\"childBAttribute\":false,\"composition\":[{\"objectType\":\"ChildAA\",\"parentAttribute\":null,\"childAAttribute\":4711,\"childAAAttribute\":0}],\"childBBAttribute\":123456789,\"deprecatedAttribute\":0,\"deprecatedBs\":[],\"deprecatedParent\":null,\"deprecatedArray\":null}",
         lValue);
 
-    lDeserializedParent = lObjectMapper.readValue(lValue, ParentClass.class);
+    lDeserializedParent = lTools.readJSON(lValue, ParentClass.class);
     assert (lDeserializedParent instanceof ChildBB);
     ChildBB lDesserializedBB = (ChildBB) lDeserializedParent;
 
