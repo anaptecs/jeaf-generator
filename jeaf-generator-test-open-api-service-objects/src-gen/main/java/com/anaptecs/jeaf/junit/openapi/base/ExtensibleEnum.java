f4226f8ac30facabaca1f2491fc9a2e7eca28b00
tillmannschall <tillmann.schall@anaptecs.de>
2022-03-28 07:00:11 +0200

    Added support for extensible enums in Java

--- jeaf-generator-test-open-api-service-objects/src-gen/main/java/com/anaptecs/jeaf/junit/openapi/base/ExtensibleEnum.java
+++ jeaf-generator-test-open-api-service-objects/src-gen/main/java/com/anaptecs/jeaf/junit/openapi/base/ExtensibleEnum.java
@@ -1,25 +1,162 @@
 package com.anaptecs.jeaf.junit.openapi.base;
 
+import com.anaptecs.jeaf.xfun.api.checks.Check;
+
 /**
  * 
  */
-public enum ExtensibleEnum {
-  /**
-   * 
-   */
-  RED,
-  /**
-   * 
-   */
-  GREEN,
-  /**
-   * 
-   */
-  BLUE;
+public class ExtensibleEnum {
+  static enum ExtensibleEnumType {
+    /**
+     * 
+     */
+    RED,
+    /**
+     * 
+     */
+    GREEN,
+    /**
+     * 
+     */
+    BLUE,
+    /**
+     * Literal UNKNOWN is used in case that an unknown literal of this enumeration is received e.g. via an external
+     * interface.
+     */
+    UNKNOWN;
+  }
 
   /**
-   * Initialize object.
+   * 
    */
-  private ExtensibleEnum( ) {
+  public static final ExtensibleEnum RED = new ExtensibleEnum(ExtensibleEnumType.RED);
+
+  /**
+   * 
+   */
+  public static final ExtensibleEnum GREEN = new ExtensibleEnum(ExtensibleEnumType.GREEN);
+
+  /**
+   * 
+   */
+  public static final ExtensibleEnum BLUE = new ExtensibleEnum(ExtensibleEnumType.BLUE);
+
+  /**
+   * Literal of the enumeration that is represented by this instance.
+   */
+  private ExtensibleEnumType literal;
+
+  /**
+   * In case that the literal is unknown then also it's actual name is still available as
+   * <code>unknownLiteralName</code>
+   */
+  private String unknownLiteralName;
+
+  /**
+   * Initialize object from real enumeration literal
+   * 
+   * @param pLiteral Enumeration literal that should be used to initialize the extensible enumeration. The parameter
+   * must not be null.
+   */
+  public ExtensibleEnum( ExtensibleEnumType pLiteral ) {
+    Check.checkInvalidParameterNull(pLiteral, "pLiteral");
+    literal = pLiteral;
+    unknownLiteralName = null;
+  }
+
+  /**
+   * Initialize object using the name of the literal.
+   * 
+   * @param pLiteralName Literal name that should be used to initialize the extensible enumeration. If the parameter is
+   * null or has an unknown literal name then a "unknown" version will be created.
+   * 
+   * @see ExtensibleEnum#isUnknownLiteral()
+   */
+  public ExtensibleEnum( String pLiteralName ) {
+    try {
+      literal = Enum.valueOf(ExtensibleEnumType.class, pLiteralName);
+      unknownLiteralName = null;
+    }
+    // Literal is unknown
+    catch (IllegalArgumentException e) {
+      literal = ExtensibleEnumType.UNKNOWN;
+      unknownLiteralName = pLiteralName;
+    }
+  }
+
+  /**
+   * Method returns the literal that is represented by this object.
+   * 
+   * @return {@link ExtensibleEnum} Literal that is represented by this object. The method never returns null.
+   */
+  public ExtensibleEnumType getLiteral( ) {
+    return literal;
+  }
+
+  /**
+   * Method checks if the represented literal is actually unknown or not.
+   * 
+   * @return boolean Method returns true if this object represents an unknown literal.
+   */
+  public boolean isUnknownLiteral( ) {
+    return literal == ExtensibleEnumType.UNKNOWN;
+  }
+
+  /**
+   * Method returns the actual name of the unknown literal
+   * 
+   * @return {@link String} Name of the unknown literal or null in case that the literal is known.
+   */
+  public String getUnknownLiteralName( ) {
+    return unknownLiteralName;
+  }
+
+  /**
+   * {@link Object#hashCode()}
+   */
+  @Override
+  public int hashCode( ) {
+    int lHashCode;
+    if (this.isUnknownLiteral() == true) {
+      lHashCode = unknownLiteralName.hashCode();
+    }
+    else {
+      lHashCode = literal.hashCode();
+    }
+    return lHashCode;
+  }
+
+  /**
+   * {@link Object#equals(Object)}
+   */
+  @Override
+  public boolean equals( Object pObject ) {
+    boolean lEquals;
+    if (pObject != null) {
+      if (this == pObject) {
+        lEquals = true;
+      }
+      // Passed object is of same type
+      else if (pObject instanceof ExtensibleEnum) {
+        ExtensibleEnum lEnum = (ExtensibleEnum) pObject;
+        // Compare if unknown literal is the same
+        if (this.isUnknownLiteral() && lEnum.isUnknownLiteral()) {
+          lEquals = this.unknownLiteralName.equals(lEnum.unknownLiteralName);
+        }
+        // Compare based on literals
+        else {
+          lEquals = this.literal == lEnum.literal;
+        }
+      }
+      // Passed object is of different type
+      else {
+        lEquals = false;
+      }
+    }
+    // Passed object is null.
+    else {
+      lEquals = false;
+    }
+    return lEquals;
   }
 }
