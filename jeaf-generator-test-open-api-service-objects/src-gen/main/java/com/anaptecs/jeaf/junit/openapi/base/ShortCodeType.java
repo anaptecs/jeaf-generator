243d70c0c8b0b0f562a47db26a3cf9aa28ff2a16
Tillmann Schall <tls@anaptecs.de>
2022-03-30 11:47:08 +0200

    Added test for OpenAPIDataTypes as separate type in OpenAPI spec

---
+++ jeaf-generator-test-open-api-service-objects/src-gen/main/java/com/anaptecs/jeaf/junit/openapi/base/ShortCodeType.java
@@ -0,0 +1,180 @@
+/*
+ * anaptecs GmbH, Ricarda-Huch-Str. 71, 72760 Reutlingen, Germany
+ * 
+ * Copyright 2004 - 2021. All rights reserved.
+ */
+package com.anaptecs.jeaf.junit.openapi.base;
+
+import javax.validation.ConstraintViolationException;
+
+import com.anaptecs.jeaf.core.api.ServiceObject;
+import com.anaptecs.jeaf.tools.api.Tools;
+import com.anaptecs.jeaf.xfun.api.XFun;
+import com.anaptecs.jeaf.xfun.api.XFunMessages;
+import com.anaptecs.jeaf.xfun.api.checks.Check;
+
+/**
+ * @author JEAF Generator
+ * @version JEAF Release 1.6.x
+ */
+public class ShortCodeType implements ServiceObject {
+  /**
+   * Default serial version uid.
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * 
+   */
+  private short code;
+
+  /**
+   * Default constructor is only intended to be used for deserialization as many frameworks required that. For "normal"
+   * object creation builder should be used instead.
+   */
+  protected ShortCodeType( ) {
+    // Nothing to do.
+  }
+
+  /**
+   * Initialize object using the passed builder.
+   * 
+   * @param pBuilder Builder that should be used to initialize this object. The parameter must not be null.
+   */
+  protected ShortCodeType( Builder pBuilder ) {
+    // Ensure that builder is not null.
+    Check.checkInvalidParameterNull(pBuilder, "pBuilder");
+    // Read attribute values from builder.
+    code = pBuilder.code;
+  }
+
+  /**
+   * Class implements builder to create a new instance of class ShortCodeType. As the class has read only attributes or
+   * associations instances can not be created directly. Instead this builder class has to be used.
+   */
+  public static class Builder {
+    /**
+     * 
+     */
+    private short code;
+
+    /**
+     * Use {@link #newBuilder()} instead of private constructor to create new builder.
+     */
+    protected Builder( ) {
+    }
+
+    /**
+     * Use {@link #newBuilder(ShortCodeType)} instead of private constructor to create new builder.
+     */
+    protected Builder( ShortCodeType pObject ) {
+      if (pObject != null) {
+        // Read attribute values from passed object.
+        code = pObject.code;
+      }
+    }
+
+    /**
+     * Method returns a new builder.
+     * 
+     * @return {@link Builder} New builder that can be used to create new ImmutablePOJOParent objects.
+     */
+    public static Builder newBuilder( ) {
+      return new Builder();
+    }
+
+    /**
+     * Method creates a new builder and initialize it with the data from the passed object.
+     * 
+     * @param pObject Object that should be used to initialize the builder. The parameter may be null.
+     * @return {@link Builder} New builder that can be used to create new ShortCodeType objects. The method never
+     * returns null.
+     */
+    public static Builder newBuilder( ShortCodeType pObject ) {
+      return new Builder(pObject);
+    }
+
+    /**
+     * Method sets the attribute "code".
+     * 
+     * @param pCode Value to which the attribute "code" should be set.
+     */
+    public Builder setCode( short pCode ) {
+      // Assign value to attribute
+      code = pCode;
+      return this;
+    }
+
+    /**
+     * Method creates a new instance of class ShortCodeType. The object will be initialized with the values of the
+     * builder.
+     * 
+     * @return ShortCodeType Created object. The method never returns null.
+     */
+    public ShortCodeType build( ) {
+      return new ShortCodeType(this);
+    }
+
+    /**
+     * Method creates a new validated instance of class ShortCodeType. The object will be initialized with the values of
+     * the builder and validated afterwards.
+     * 
+     * @return ShortCodeType Created and validated object. The method never returns null.
+     * @throws ConstraintViolationException in case that one or more validations for the created object failed.
+     */
+    public ShortCodeType buildValidated( ) throws ConstraintViolationException {
+      ShortCodeType lPOJO = this.build();
+      Tools.getValidationTools().enforceObjectValidation(lPOJO);
+      return lPOJO;
+    }
+  }
+
+  /**
+   * Method returns the attribute "code".
+   * 
+   * 
+   * @return short Value to which the attribute "code" is set.
+   */
+  public short getCode( ) {
+    return code;
+  }
+
+  /**
+   * Method sets the attribute "code".
+   * 
+   * 
+   * @param pCode Value to which the attribute "code" should be set.
+   */
+  public void setCode( short pCode ) {
+    // Assign value to attribute
+    code = pCode;
+  }
+
+  /**
+   * Method returns a StringBuilder that can be used to create a String representation of this object. the returned
+   * StringBuilder also takes care about attributes of super classes.
+   *
+   * @return {@link StringBuilder} StringBuilder representing this object. The method never returns null.
+   */
+  protected StringBuilder toStringBuilder( ) {
+    StringBuilder lBuilder = new StringBuilder(256);
+    lBuilder.append(XFun.getMessageRepository().getMessage(XFunMessages.OBJECT_INFO, this.getClass().getName()));
+    lBuilder.append('\n');
+    lBuilder.append(XFun.getMessageRepository().getMessage(XFunMessages.OBJECT_ATTRIBUTES_SECTION));
+    lBuilder.append('\n');
+    lBuilder.append(XFun.getMessageRepository().getMessage(XFunMessages.OBJECT_ATTRIBUTE, "code", "" + code));
+    lBuilder.append('\n');
+    return lBuilder;
+  }
+
+  /**
+   * Method creates a new String with the values of all attributes of this class. All references to other objects will
+   * be ignored.
+   * 
+   * @return {@link String} String representation of this object. The method never returns null.
+   */
+  @Override
+  public String toString( ) {
+    return this.toStringBuilder().toString();
+  }
+}
---
+++ jeaf-generator-test-open-api-service-objects/src-gen/main/java/com/anaptecs/jeaf/junit/openapi/base/serializers/ByteCodeTypeSerializer.java
@@ -0,0 +1,25 @@
+/*
+ * anaptecs GmbH, Ricarda-Huch-Str. 71, 72760 Reutlingen, Germany
+ * 
+ * Copyright 2004 - 2021. All rights reserved.
+ */
+package com.anaptecs.jeaf.junit.openapi.base.serializers;
+
+import java.io.IOException;
+
+import com.anaptecs.jeaf.junit.openapi.base.ByteCodeType;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+/**
+ * Class implements a JSON / Jackson serializer for class ByteCodeType.
+ */
+public class ByteCodeTypeSerializer extends JsonSerializer<ByteCodeType> {
+  @Override
+  public void serialize( ByteCodeType pObject, JsonGenerator pGenerator, SerializerProvider pSerializers )
+    throws IOException {
+    // Write value directly to JSON and avoid nesting object structures
+    pGenerator.writeNumber(pObject.getCode());
+  }
+}
