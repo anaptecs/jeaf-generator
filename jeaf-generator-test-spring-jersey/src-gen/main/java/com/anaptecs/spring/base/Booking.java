89ea99a30b760b48b365525b9575bd0c19c50bf2
JEAF Development Team <tls@anaptecs.de>
2022-12-02 15:17:02 +0100

    Fixed JEAF-3149

--- jeaf-generator-test-spring-jersey/src-gen/main/java/com/anaptecs/spring/base/Booking.java
+++ jeaf-generator-test-spring-jersey/src-gen/main/java/com/anaptecs/spring/base/Booking.java
@@ -176,7 +176,7 @@ public class Booking {
     public Builder setInventories( InventoryType... pInventories ) {
       // To ensure immutability we have to copy the content of the passed array.
       if (pInventories != null) {
-        inventories = Arrays.asList(pInventories);
+        inventories = new ArrayList<InventoryType>(Arrays.asList(pInventories));
       }
       else {
         inventories = null;
