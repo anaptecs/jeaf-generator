8dd9d9febf572da0f6f4cdc348d7f3fae21061ce
JEAF Development Team <tls@anaptecs.de>
2023-03-06 07:17:49 +0100

    Prepared tests

---
+++ jeaf-generator-test-open-api-service-objects/src-gen/main/java/com/anaptecs/jeaf/junit/openapi/techbase/BusinessParent.java
@@ -0,0 +1,195 @@
+/*
+ * anaptecs GmbH, Ricarda-Huch-Str. 71, 72760 Reutlingen, Germany
+ * 
+ * Copyright 2004 - 2021. All rights reserved.
+ */
+package com.anaptecs.jeaf.junit.openapi.techbase;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+
+import com.anaptecs.jeaf.tools.api.validation.ValidationTools;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * @author JEAF Generator
+ * @version JEAF Release 1.6.x
+ */
+@Valid
+@JsonIgnoreProperties(value = "objectType")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "objectType", visible = true)
+@JsonSubTypes({ @JsonSubTypes.Type(value = BusinessChild.class, name = "BusinessChild") })
+public class BusinessParent extends TechParent {
+  /**
+   * Default serial version uid.
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * 
+   */
+  private long parentAttribute;
+
+  /**
+   * Default constructor is only intended to be used for deserialization by tools like Jackson for JSON. For "normal"
+   * object creation builder should be used instead.
+   */
+  protected BusinessParent( ) {
+  }
+
+  /**
+   * Initialize object using the passed builder.
+   * 
+   * @param pBuilder Builder that should be used to initialize this object. The parameter must not be null.
+   */
+  protected BusinessParent( Builder pBuilder ) {
+    // Call constructor of super class.
+    super(pBuilder);
+    // Read attribute values from builder.
+    parentAttribute = pBuilder.parentAttribute;
+  }
+
+  /**
+   * Method returns a new builder.
+   * 
+   * @return {@link Builder} New builder that can be used to create new BusinessParent objects.
+   */
+  public static Builder builder( ) {
+    return new Builder();
+  }
+
+  /**
+   * Method creates a new builder and initialize it with the data from the passed object.
+   * 
+   * @param pObject Object that should be used to initialize the builder. The parameter may be null.
+   * @return {@link Builder} New builder that can be used to create new BusinessParent objects. The method never returns
+   * null.
+   */
+  public static Builder builder( BusinessParent pObject ) {
+    return new Builder(pObject);
+  }
+
+  /**
+   * Class implements builder to create a new instance of class BusinessParent. As the class has read only attributes or
+   * associations instances can not be created directly. Instead this builder class has to be used.
+   */
+  public static class Builder extends TechParent.Builder {
+    /**
+     * 
+     */
+    private long parentAttribute;
+
+    /**
+     * Use {@link BusinessParent#builder()} instead of private constructor to create new builder.
+     */
+    protected Builder( ) {
+      super();
+    }
+
+    /**
+     * Use {@link BusinessParent#builder(BusinessParent)} instead of private constructor to create new builder.
+     */
+    protected Builder( BusinessParent pObject ) {
+      super(pObject);
+      if (pObject != null) {
+        // Read attribute values from passed object.
+        parentAttribute = pObject.parentAttribute;
+      }
+    }
+
+    /**
+     * Method sets the attribute "techAttribute".
+     * 
+     * @param pTechAttribute Value to which the attribute "techAttribute" should be set.
+     */
+    @Override
+    public Builder setTechAttribute( String pTechAttribute ) {
+      // Call super class implementation.
+      super.setTechAttribute(pTechAttribute);
+      return this;
+    }
+
+    /**
+     * Method sets the attribute "parentAttribute".
+     * 
+     * @param pParentAttribute Value to which the attribute "parentAttribute" should be set.
+     */
+    public Builder setParentAttribute( long pParentAttribute ) {
+      // Assign value to attribute
+      parentAttribute = pParentAttribute;
+      return this;
+    }
+
+    /**
+     * Method creates a new instance of class BusinessParent. The object will be initialized with the values of the
+     * builder.
+     * 
+     * @return BusinessParent Created object. The method never returns null.
+     */
+    public BusinessParent build( ) {
+      return new BusinessParent(this);
+    }
+
+    /**
+     * Method creates a new validated instance of class BusinessParent. The object will be initialized with the values
+     * of the builder and validated afterwards.
+     * 
+     * @return BusinessParent Created and validated object. The method never returns null.
+     * @throws ConstraintViolationException in case that one or more validations for the created object failed.
+     */
+    public BusinessParent buildValidated( ) throws ConstraintViolationException {
+      BusinessParent lPOJO = this.build();
+      ValidationTools.getValidationTools().enforceObjectValidation(lPOJO);
+      return lPOJO;
+    }
+  }
+
+  /**
+   * Method returns the attribute "parentAttribute".
+   * 
+   * 
+   * @return long Value to which the attribute "parentAttribute" is set.
+   */
+  public long getParentAttribute( ) {
+    return parentAttribute;
+  }
+
+  /**
+   * Method sets the attribute "parentAttribute".
+   * 
+   * 
+   * @param pParentAttribute Value to which the attribute "parentAttribute" should be set.
+   */
+  public void setParentAttribute( long pParentAttribute ) {
+    // Assign value to attribute
+    parentAttribute = pParentAttribute;
+  }
+
+  /**
+   * Method returns a StringBuilder that can be used to create a String representation of this object. The returned
+   * StringBuilder also takes care about attributes of super classes.
+   *
+   * @return {@link StringBuilder} StringBuilder representing this object. The method never returns null.
+   */
+  public StringBuilder toStringBuilder( String pIndent ) {
+    StringBuilder lBuilder = super.toStringBuilder(pIndent);
+    lBuilder.append(pIndent);
+    lBuilder.append("parentAttribute: ");
+    lBuilder.append(parentAttribute);
+    lBuilder.append(System.lineSeparator());
+    return lBuilder;
+  }
+
+  /**
+   * Method creates a new String with the values of all attributes of this class. All references to other objects will
+   * be ignored.
+   * 
+   * @return {@link String} String representation of this object. The method never returns null.
+   */
+  @Override
+  public String toString( ) {
+    return this.toStringBuilder("").toString();
+  }
+}
