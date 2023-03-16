aeeacde7402cb019f95f374474291b8ad6866cfc
JEAF Development Team <tls@anaptecs.de>
2023-03-16 07:45:23 +0100

    Prepare model for derived properties

--- jeaf-generator-test-spring-jersey/src-gen/main/java/com/anaptecs/spring/base/MasterDataObject.java
+++ jeaf-generator-test-spring-jersey/src-gen/main/java/com/anaptecs/spring/base/MasterDataObject.java
@@ -9,7 +9,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -40,6 +42,16 @@ public class MasterDataObject {
    */
   public static final String DERIVEDPROPERTY = "derivedProperty";
 
+  /**
+   * Constant for the name of attribute "derivedDataUnits".
+   */
+  public static final String DERIVEDDATAUNITS = "derivedDataUnits";
+
+  /**
+   * Constant for the name of attribute "derivedEntity".
+   */
+  public static final String DERIVEDENTITY = "derivedEntity";
+
   private List<DataUnit> dataUnits;
 
   private Entity entity;
@@ -50,12 +62,18 @@ public class MasterDataObject {
 
   private String derivedProperty;
 
+  private List<DataUnit> derivedDataUnits;
+
+  private Set<Entity> derivedEntity;
+
   /**
    * Default constructor is only intended to be used for deserialization by tools like Jackson for JSON. For "normal"
    * object creation builder should be used instead.
    */
   protected MasterDataObject( ) {
     dataUnits = new ArrayList<DataUnit>();
+    derivedDataUnits = new ArrayList<DataUnit>();
+    derivedEntity = new HashSet<Entity>();
   }
 
   /**
@@ -75,6 +93,18 @@ public class MasterDataObject {
     objectID = pBuilder.objectID;
     internalProperty = pBuilder.internalProperty;
     derivedProperty = pBuilder.derivedProperty;
+    if (pBuilder.derivedDataUnits != null) {
+      derivedDataUnits = pBuilder.derivedDataUnits;
+    }
+    else {
+      derivedDataUnits = new ArrayList<DataUnit>();
+    }
+    if (pBuilder.derivedEntity != null) {
+      derivedEntity = pBuilder.derivedEntity;
+    }
+    else {
+      derivedEntity = new HashSet<Entity>();
+    }
   }
 
   /**
@@ -111,6 +141,10 @@ public class MasterDataObject {
 
     private String derivedProperty;
 
+    private List<DataUnit> derivedDataUnits;
+
+    private Set<Entity> derivedEntity;
+
     /**
      * Use {@link MasterDataObject#builder()} instead of private constructor to create new builder.
      */
@@ -128,6 +162,8 @@ public class MasterDataObject {
         objectID = pObject.objectID;
         internalProperty = pObject.internalProperty;
         derivedProperty = pObject.derivedProperty;
+        derivedDataUnits = pObject.derivedDataUnits;
+        derivedEntity = pObject.derivedEntity;
       }
     }
 
@@ -209,6 +245,74 @@ public class MasterDataObject {
     public Builder setDerivedProperty( String pDerivedProperty ) {
       // Assign value to attribute
       derivedProperty = pDerivedProperty;
+      return this;
+    }
+
+    /**
+     * Method sets association {@link #derivedDataUnits}.<br/>
+     * 
+     * @param pDerivedDataUnits Collection to which {@link #derivedDataUnits} should be set.
+     * @return {@link Builder} Instance of this builder to support chaining setters. Method never returns null.
+     */
+    public Builder setDerivedDataUnits( List<DataUnit> pDerivedDataUnits ) {
+      // To ensure immutability we have to copy the content of the passed collection.
+      if (pDerivedDataUnits != null) {
+        derivedDataUnits = new ArrayList<DataUnit>(pDerivedDataUnits);
+      }
+      else {
+        derivedDataUnits = null;
+      }
+      return this;
+    }
+
+    /**
+     * Method sets association {@link #derivedDataUnits}.<br/>
+     * 
+     * @param pDerivedDataUnits Array with objects to which {@link #derivedDataUnits} should be set.
+     * @return {@link Builder} Instance of this builder to support chaining setters. Method never returns null.
+     */
+    public Builder setDerivedDataUnits( DataUnit... pDerivedDataUnits ) {
+      // To ensure immutability we have to copy the content of the passed array.
+      if (pDerivedDataUnits != null) {
+        derivedDataUnits = new ArrayList<DataUnit>(Arrays.asList(pDerivedDataUnits));
+      }
+      else {
+        derivedDataUnits = null;
+      }
+      return this;
+    }
+
+    /**
+     * Method sets association {@link #derivedEntity}.<br/>
+     * 
+     * @param pDerivedEntity Collection to which {@link #derivedEntity} should be set.
+     * @return {@link Builder} Instance of this builder to support chaining setters. Method never returns null.
+     */
+    public Builder setDerivedEntity( Set<Entity> pDerivedEntity ) {
+      // To ensure immutability we have to copy the content of the passed collection.
+      if (pDerivedEntity != null) {
+        derivedEntity = new HashSet<Entity>(pDerivedEntity);
+      }
+      else {
+        derivedEntity = null;
+      }
+      return this;
+    }
+
+    /**
+     * Method sets association {@link #derivedEntity}.<br/>
+     * 
+     * @param pDerivedEntity Array with objects to which {@link #derivedEntity} should be set.
+     * @return {@link Builder} Instance of this builder to support chaining setters. Method never returns null.
+     */
+    public Builder setDerivedEntity( Entity... pDerivedEntity ) {
+      // To ensure immutability we have to copy the content of the passed array.
+      if (pDerivedEntity != null) {
+        derivedEntity = new HashSet<Entity>(Arrays.asList(pDerivedEntity));
+      }
+      else {
+        derivedEntity = null;
+      }
       return this;
     }
 
@@ -358,6 +462,111 @@ public class MasterDataObject {
   }
 
   /**
+   * Method returns association {@link #derivedDataUnits}.<br/>
+   * 
+   * @return {@link List<DataUnit>} Value to which {@link #derivedDataUnits} is set. The method never returns null and
+   * the returned collection is unmodifiable.
+   */
+  public List<DataUnit> getDerivedDataUnits( ) {
+    // Return all DataUnit objects as unmodifiable collection.
+    return Collections.unmodifiableList(derivedDataUnits);
+  }
+
+  /**
+   * Method adds the passed object to {@link #derivedDataUnits}.
+   * 
+   * @param pDerivedDataUnits Object that should be added to {@link #derivedDataUnits}. The parameter must not be null.
+   */
+  public void addToDerivedDataUnits( DataUnit pDerivedDataUnits ) {
+    // Add passed object to collection of associated DataUnit objects.
+    derivedDataUnits.add(pDerivedDataUnits);
+  }
+
+  /**
+   * Method adds all passed objects to {@link #derivedDataUnits}.
+   * 
+   * @param pDerivedDataUnits Collection with all objects that should be added to {@link #derivedDataUnits}. The
+   * parameter must not be null.
+   */
+  public void addToDerivedDataUnits( Collection<DataUnit> pDerivedDataUnits ) {
+    // Add all passed objects.
+    for (DataUnit lNextObject : pDerivedDataUnits) {
+      this.addToDerivedDataUnits(lNextObject);
+    }
+  }
+
+  /**
+   * Method removes the passed object from {@link #derivedDataUnits}.<br/>
+   * 
+   * @param pDerivedDataUnits Object that should be removed from {@link #derivedDataUnits}. The parameter must not be
+   * null.
+   */
+  public void removeFromDerivedDataUnits( DataUnit pDerivedDataUnits ) {
+    // Remove passed object from collection of associated DataUnit objects.
+    derivedDataUnits.remove(pDerivedDataUnits);
+  }
+
+  /**
+   * Method removes all objects from {@link #derivedDataUnits}.
+   */
+  public void clearDerivedDataUnits( ) {
+    // Remove all objects from association "derivedDataUnits".
+    derivedDataUnits.clear();
+  }
+
+  /**
+   * Method returns association {@link #derivedEntity}.<br/>
+   * 
+   * @return {@link Set<Entity>} Value to which {@link #derivedEntity} is set. The method never returns null and the
+   * returned collection is unmodifiable.
+   */
+  public Set<Entity> getDerivedEntity( ) {
+    // Return all Entity objects as unmodifiable collection.
+    return Collections.unmodifiableSet(derivedEntity);
+  }
+
+  /**
+   * Method adds the passed object to {@link #derivedEntity}.
+   * 
+   * @param pDerivedEntity Object that should be added to {@link #derivedEntity}. The parameter must not be null.
+   */
+  public void addToDerivedEntity( Entity pDerivedEntity ) {
+    // Add passed object to collection of associated Entity objects.
+    derivedEntity.add(pDerivedEntity);
+  }
+
+  /**
+   * Method adds all passed objects to {@link #derivedEntity}.
+   * 
+   * @param pDerivedEntity Collection with all objects that should be added to {@link #derivedEntity}. The parameter
+   * must not be null.
+   */
+  public void addToDerivedEntity( Collection<Entity> pDerivedEntity ) {
+    // Add all passed objects.
+    for (Entity lNextObject : pDerivedEntity) {
+      this.addToDerivedEntity(lNextObject);
+    }
+  }
+
+  /**
+   * Method removes the passed object from {@link #derivedEntity}.<br/>
+   * 
+   * @param pDerivedEntity Object that should be removed from {@link #derivedEntity}. The parameter must not be null.
+   */
+  public void removeFromDerivedEntity( Entity pDerivedEntity ) {
+    // Remove passed object from collection of associated Entity objects.
+    derivedEntity.remove(pDerivedEntity);
+  }
+
+  /**
+   * Method removes all objects from {@link #derivedEntity}.
+   */
+  public void clearDerivedEntity( ) {
+    // Remove all objects from association "derivedEntity".
+    derivedEntity.clear();
+  }
+
+  /**
    * Method returns a StringBuilder that can be used to create a String representation of this object. The returned
    * StringBuilder also takes care about attributes of super classes.
    *
@@ -401,6 +610,40 @@ public class MasterDataObject {
     lBuilder.append("derivedProperty: ");
     lBuilder.append(derivedProperty);
     lBuilder.append(System.lineSeparator());
+    lBuilder.append(pIndent);
+    lBuilder.append("derivedDataUnits: ");
+    if (derivedDataUnits != null) {
+      lBuilder.append(derivedDataUnits.size());
+      lBuilder.append(" element(s)");
+    }
+    else {
+      lBuilder.append(" null");
+    }
+    lBuilder.append(System.lineSeparator());
+    if (derivedDataUnits != null) {
+      for (DataUnit lNext : derivedDataUnits) {
+        lBuilder.append(pIndent + "    ");
+        lBuilder.append(lNext.toString());
+        lBuilder.append(System.lineSeparator());
+      }
+    }
+    lBuilder.append(pIndent);
+    lBuilder.append("derivedEntity: ");
+    if (derivedEntity != null) {
+      lBuilder.append(derivedEntity.size());
+      lBuilder.append(" element(s)");
+    }
+    else {
+      lBuilder.append(" null");
+    }
+    lBuilder.append(System.lineSeparator());
+    if (derivedEntity != null) {
+      for (Entity lNext : derivedEntity) {
+        lBuilder.append(pIndent + "    ");
+        lBuilder.append(lNext.toString());
+        lBuilder.append(System.lineSeparator());
+      }
+    }
     return lBuilder;
   }
 
