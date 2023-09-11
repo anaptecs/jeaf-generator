a780b075cf6c6e81b41678d21b64f96ed9804055
JEAF Development Team <jeaf@anaptecs.de>
2023-09-11 06:50:02 +0200

    Added info about authorized roles to Javadoc of services.

--- jeaf-generator-test-spring/src-gen/main/java/com/anaptecs/spring/service/RESTProductService.java
+++ jeaf-generator-test-spring/src-gen/main/java/com/anaptecs/spring/service/RESTProductService.java
@@ -43,6 +43,8 @@ import com.anaptecs.spring.base.TimeUnit;
 public interface RESTProductService {
   /**
    * Operation returns all available product.
+   * <p/>
+   * <b>Authorized Roles:</b> Customer, Sales Agent
    *
    * @param pMaxResultSize
    * @return {@link List<Product>}
@@ -50,50 +52,75 @@ public interface RESTProductService {
   List<Product> getProducts( int pMaxResultSize );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pProductID
    * @return {@link Product}
    */
   Product getProduct( @NotEmpty String pProductID );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pProduct
    * @return boolean
    */
   boolean createProduct( Product pProduct );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pContext
    * @return {@link Sortiment}
    */
   Sortiment getSortiment( Context pContext );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pChannelCode
    * @return {@link ChannelCode}
    */
   ChannelCode createChannelCode( @NotBlank String pChannelCode );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Customer, Sales Agent
    */
   void ping( );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
    */
   void testInit( );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pChannelCode
    * @return {@link List<CurrencyCode>}
    */
   List<CurrencyCode> getSupportedCurrencies( ChannelCode pChannelCode );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pChannelCode
    * @return {@link List<CurrencyCode>}
    */
   List<CurrencyCode> getSupportedCurrenciesAsync( ChannelCode pChannelCode );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pBigDecimalHeader
    * @param pIntCookieParam
    * @param pLocaleQueryParam
@@ -102,6 +129,9 @@ public interface RESTProductService {
   String testParams( BigDecimal pBigDecimalHeader, int pIntCookieParam, Locale pLocaleQueryParam );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pChannelType
    * @param pTimeUnit
    * @param pExtensibleEnum
@@ -109,6 +139,9 @@ public interface RESTProductService {
   void testEnumParams( ChannelType pChannelType, TimeUnit pTimeUnit, ExtensibleEnum pExtensibleEnum );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pChannelType
    * @param pTimeUnit
    * @param pExtensibleEnum
@@ -116,6 +149,9 @@ public interface RESTProductService {
   void testEnumHeaderParams( ChannelType pChannelType, TimeUnit pTimeUnit, ExtensibleEnum pExtensibleEnum );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pPath
    * @param pStartTimestamp
    * @param pStartTime
@@ -134,12 +170,18 @@ public interface RESTProductService {
       java.util.Date pUtilDate, Timestamp pSQLTimestamp, Time pSQLTime, Date pSQLDate, Set<Calendar> pCalendars );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pPath
    * @param pQueryParams
    */
   void testDateQueryParamsBean( String pPath, DateQueryParamsBean pQueryParams );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pPath
    * @param pOffsetDateTime
    * @param pOffsetTime
@@ -158,18 +200,27 @@ public interface RESTProductService {
       java.util.Date pUtilDate, Timestamp pSQLTimestamp, Time pSQLTime, Date pSQLDate, Set<java.util.Date> pUtilDates );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pPath
    * @param pHeaderParams
    */
   void testDateHeaderParamsBean( String pPath, DateHeaderParamsBean pHeaderParams );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pChannelTypeParam
    * @param pContext
    */
   void testCookieParams( ChannelType pChannelTypeParam, SpecialContext pContext );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param query1
    * @param query2
    * @return {@link String}
@@ -177,12 +228,18 @@ public interface RESTProductService {
   String testOptionalQueryParams( String query1, int query2 );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pComplextBookingID
    * @return boolean
    */
   boolean processComplexBookingID( ComplexBookingID pComplextBookingID );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pBookingID
    * @param pBookingCode
    * @param pDoubleCode
@@ -191,54 +248,81 @@ public interface RESTProductService {
   String testDataTypesAsHeaderParam( BookingID pBookingID, BookingCode pBookingCode, DoubleCode pDoubleCode );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pContext
    * @return {@link String}
    */
   String testDataTypesAsHeaderBeanParam( AdvancedHeader pContext );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pIntegerArray
    * @return {@link String}
    */
   String testPrimitiveArrays( int[] pIntegerArray );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pBookingCode
    * @return {@link String}
    */
   String testDataTypeAsQueryParam( BookingCode pBookingCode );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pBeanParam
    * @return {@link String}
    */
   String testDataTypeAsBeanQueryParam( QueryBeanParam pBeanParam );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pIntValues
    * @return {@link String}
    */
   String testPrimitiveArrayAsQueryParam( int[] pIntValues );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pStrings
    * @return {@link String}
    */
   String testSimpleTypesAsQueryParams( List<String> pStrings );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pIntegers
    * @return {@link String}
    */
   String testPrimitiveWrapperArrayAsQueryParam( Set<Integer> pIntegers );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pBean
    * @return {@link String}
    */
   String testMultivaluedQueryParamsBean( MultivaluedQueryParamsBean pBean );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pCodes
    * @param pLongCodes
    * @param pBookingIDs
@@ -250,18 +334,27 @@ public interface RESTProductService {
       List<BookingID> pBookingIDs, List<OffsetDateTime> pTimestamps, SortedSet<LocalDate> pLocalDates );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pQueryBean
    * @return {@link String}
    */
   String testMulitvaluedDataTypeAsBeanQueryParam( DataTypesQueryBean pQueryBean );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pMultiValuedBean
    * @return {@link String}
    */
   String testMultiValuedHeaderFieldsInBeanParam( MultiValuedHeaderBeanParam pMultiValuedBean );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pNames
    * @param pInts
    * @param pDoubles
@@ -275,11 +368,17 @@ public interface RESTProductService {
       OffsetDateTime pStartDate, Set<OffsetDateTime> pTimestamps, Set<OffsetTime> pTimes );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pBookingID
    */
   void testBookingIDAsPathParam( BookingID pBookingID );
 
   /**
+   * <p/>
+   * <b>Authorized Roles:</b> Sales Agent
+   *
    * @param pBookingID
    */
   void testBookingIDAsHeaderParam( BookingID pBookingID );
