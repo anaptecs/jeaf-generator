bd830b887b30285af993300614b8fb88c4c3fb44
jeaf@anaptecs.de <114045600+jeaf-anaptecs@users.noreply.github.com>
2024-07-04 18:34:23 +0200

    #229 jsonsetter only required for collections (#230)
    * Fixed formatting issue in security roles report
    
    * Special setter only generated for multivalued properties

--- jeaf-generator-test-jeaf-rest-proxy/src-gen/main/java/com/anaptecs/spring/base/MasterDataObjectBase.java
+++ jeaf-generator-test-jeaf-rest-proxy/src-gen/main/java/com/anaptecs/spring/base/MasterDataObjectBase.java
@@ -388,7 +388,7 @@ public abstract class MasterDataObjectBase implements Serializable {
   public abstract List<DataUnit> getDerivedDataUnits( );
 
   @JsonSetter
-  public void setDerivedDataUnits( List<DataUnit> pDerivedDataUnits ) {
+  private void setDerivedDataUnits( List<DataUnit> pDerivedDataUnits ) {
     // Actively ignore passed objects as this is just a derived property. Unfortunately there is no more elegant variant
     // to not run into problems during deserialization.
   }
@@ -400,12 +400,6 @@ public abstract class MasterDataObjectBase implements Serializable {
    */
   @JsonGetter
   public abstract Entity getDerivedEntity( );
-
-  @JsonSetter
-  public void setDerivedEntity( Entity pDerivedEntity ) {
-    // Actively ignore passed objects as this is just a derived property. Unfortunately there is no more elegant variant
-    // to not run into problems during deserialization.
-  }
 
   /**
    * Method returns attribute {@link #derivedArray}.<br/>
--- jeaf-generator-test-spring/src-gen/main/java/com/anaptecs/spring/base/MasterDataObjectBase.java
+++ jeaf-generator-test-spring/src-gen/main/java/com/anaptecs/spring/base/MasterDataObjectBase.java
@@ -367,7 +367,7 @@ public abstract class MasterDataObjectBase {
   public abstract List<DataUnit> getDerivedDataUnits( );
 
   @JsonSetter
-  public void setDerivedDataUnits( List<DataUnit> pDerivedDataUnits ) {
+  private void setDerivedDataUnits( List<DataUnit> pDerivedDataUnits ) {
     // Actively ignore passed objects as this is just a derived property. Unfortunately there is no more elegant variant
     // to not run into problems during deserialization.
   }
@@ -379,12 +379,6 @@ public abstract class MasterDataObjectBase {
    */
   @JsonGetter
   public abstract Entity getDerivedEntity( );
-
-  @JsonSetter
-  public void setDerivedEntity( Entity pDerivedEntity ) {
-    // Actively ignore passed objects as this is just a derived property. Unfortunately there is no more elegant variant
-    // to not run into problems during deserialization.
-  }
 
   /**
    * Method returns attribute {@link #derivedArray}.<br/>
