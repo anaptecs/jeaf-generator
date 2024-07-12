51d18f9f3df4cd5c07664f6ee80c3553e58b8d58
JEAF Development Team <jeaf@anaptecs.de>
2024-07-12 05:51:51 +0200

    Updated libraries used by import beautifier

--- jeaf-generator-beautifier/src/main/java/de/plushnikov/doctorjim/ImportProcessor.java
+++ jeaf-generator-beautifier/src/main/java/de/plushnikov/doctorjim/ImportProcessor.java
@@ -10,7 +10,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
