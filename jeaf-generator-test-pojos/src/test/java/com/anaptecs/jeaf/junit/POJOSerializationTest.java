e73d4206d62a2cd5c5dbeba61f574383a514c9e8
JEAF Development Team <jeaf@anaptecs.de>
2025-05-23 09:08:37 +0200

    Updated

--- jeaf-generator-test-pojos/src/test/java/com/anaptecs/jeaf/junit/POJOSerializationTest.java
+++ jeaf-generator-test-pojos/src/test/java/com/anaptecs/jeaf/junit/POJOSerializationTest.java
@@ -3,12 +3,13 @@ package com.anaptecs.jeaf.junit;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-
 import com.anaptecs.jeaf.json.api.JSON;
 import com.anaptecs.jeaf.json.api.JSONTools;
+import com.anaptecs.jeaf.junit.genericbuilder.Child;
+import com.anaptecs.jeaf.junit.genericbuilder.Parent;
 import com.anaptecs.jeaf.junit.pojo.BidirectionalA;
 import com.anaptecs.jeaf.junit.pojo.BidirectionalB;
+import org.junit.jupiter.api.Test;
 
 public class POJOSerializationTest {
 
@@ -34,6 +35,32 @@ public class POJOSerializationTest {
     BidirectionalB lDeserializedB = lTools.read(lJSON, BidirectionalB.class);
     BidirectionalA lA = lDeserializedB.getAs().iterator().next();
     assertNotNull(lA);
+  }
+
+  @Test
+  void testGenericBuilderUsage( ) {
+    Parent lParent = Parent.builder()
+        .setPropertyA("A")
+        .build();
+
+    JSONTools lTools = JSON.getJSONTools();
+    String lJSON = lTools.writeObjectToString(lParent);
+    assertEquals("{\"propertyA\":\"A\"}", lJSON);
+
+    Parent lReadParent = lTools.read(lJSON, Parent.class);
+    assertEquals(lParent.getPropertyA(), lReadParent.getPropertyA());
+
+    Child lChild = Child.builder()
+        .setPropertyA("A*")
+        .setPropertyB(4711)
+        .build();
+
+    lJSON = lTools.writeObjectToString(lChild);
+    assertEquals("{\"propertyA\":\"A*\",\"propertyB\":4711}", lJSON);
+
+    Child lReadChild = lTools.read(lJSON, Child.class);
+    assertEquals(lChild.getPropertyA(), lReadChild.getPropertyA());
+    assertEquals(lChild.getPropertyB(), lReadChild.getPropertyB());
   }
 
 }
