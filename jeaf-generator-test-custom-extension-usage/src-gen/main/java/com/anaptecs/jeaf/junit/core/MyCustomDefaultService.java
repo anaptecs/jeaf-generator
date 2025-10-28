a66a11515ff15b31127ac8016854815b0f27dbaf
jeaf@anaptecs.de <114045600+jeaf-anaptecs@users.noreply.github.com>
2025-10-28 08:56:19 +0100

    Implemented and tested #576 (#606)
    * Implemented and tested #576
    
    * Update to latest released model version.
    
    * Update to latest parent POM.

---
+++ jeaf-generator-test-custom-extension-usage/src-gen/main/java/com/anaptecs/jeaf/junit/core/MyCustomDefaultService.java
@@ -0,0 +1,13 @@
+/*
+ * anaptecs GmbH, Ricarda-Huch-Str. 71, 72760 Reutlingen, Germany
+ *
+ * Copyright 2004 - 2021. All rights reserved.
+ */
+package com.anaptecs.jeaf.junit.core;
+
+import com.anaptecs.jeaf.junit.extension.JEAFCustomAnnotationTest;
+
+@JEAFCustomAnnotationTest
+public interface MyCustomDefaultService extends MyCustomDefaultServiceBase {
+  // TODO: Add custom default implementation for your operations here.
+}
---
+++ jeaf-generator-test-junit-testcases/src-gen/main/java/com/anaptecs/jeaf/junit/core/test/MyCustomDefaultServiceTestBase.java
@@ -0,0 +1,58 @@
+/*
+ * anaptecs GmbH, Ricarda-Huch-Str. 71, 72760 Reutlingen, Germany
+ *
+ * Copyright 2004 - 2019. All rights reserved.
+ */
+package com.anaptecs.jeaf.junit.core.test;
+
+import com.anaptecs.jeaf.core.annotations.JEAFService;
+import com.anaptecs.jeaf.junit.JEAFTestCase;
+import com.anaptecs.jeaf.junit.core.MyCustomDefaultService;
+
+/**
+ * This is the base class of the test suite of service MyCustomDefaultService.
+ *
+ * @version $LastChangedRevision: 931 $
+ */
+public abstract class MyCustomDefaultServiceTestBase extends JEAFTestCase {
+  /**
+   * Service that should be tested by this test case.
+   */
+  @JEAFService
+  protected MyCustomDefaultService myCustomDefaultService;
+
+  /**
+   * Initialize test case. Therefore the name of the test that should be executed must be provided.
+   *
+   * @param pName Name of the test method that should be executed by this test. The Parameter must not be null.
+   */
+  public MyCustomDefaultServiceTestBase( String pName ) {
+    super(pName);
+  }
+
+  /**
+   * Initialize test case. Therefore the name of the test that should be executed must be provided. The userId and the
+   * password of a specific user can be given to this Constructor so this specific user can be logged in.
+   *
+   * @param pName Name of the test method that should be executed by this test. The Parameter must not be null.
+   * @param pUserId UserId of the User who should be logged in for this Test.
+   * @param pPassword Password of the User who should be logged in for this Test.
+   */
+  public MyCustomDefaultServiceTestBase( String pName, String pUserId, String pPassword ) {
+    super(pName, pUserId, pPassword);
+  }
+
+  /**
+   * Method test the implementation of service method doSomeMore(...)
+   *
+   * @throws Exception if the test fails.
+   */
+  public abstract void testDoSomeMore( ) throws Exception;
+
+  /**
+   * Method test the implementation of service method doSomething(...)
+   *
+   * @throws Exception if the test fails.
+   */
+  public abstract void testDoSomething( ) throws Exception;
+}
