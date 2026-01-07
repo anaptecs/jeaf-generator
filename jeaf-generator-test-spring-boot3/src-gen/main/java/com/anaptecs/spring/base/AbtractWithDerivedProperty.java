1a8e656df285c19ff3fee3bbf879d561ff4c5a51
JEAF Development Team <jeaf@anaptecs.de>
2026-01-07 08:43:52 +0100

    Implemented and tested #492

---
+++ jeaf-generator-test-spring-boot3/src-gen/main/java/com/anaptecs/spring/base/AbtractWithDerivedProperty.java
@@ -0,0 +1,83 @@
+/*
+ * anaptecs GmbH, Ricarda-Huch-Str. 71, 72760 Reutlingen, Germany
+ *
+ * Copyright 2004 - 2019. All rights reserved.
+ */
+package com.anaptecs.spring.base;
+
+public abstract class AbtractWithDerivedProperty {
+  /**
+   * Initialize object using the passed builder.
+   *
+   * @param pBuilder Builder that should be used to initialize this object. The parameter must not be null.
+   */
+  protected AbtractWithDerivedProperty( Builder pBuilder ) {
+    // Read attribute values from builder.
+  }
+
+  /**
+   * Class implements builder to create a new instance of class <code>AbtractWithDerivedProperty</code>.
+   */
+  public static abstract class Builder {
+    /**
+     * Use {@link AbtractWithDerivedProperty#builder()} instead of private constructor to create new builder.
+     */
+    protected Builder( ) {
+    }
+
+    /**
+     * Use {@link AbtractWithDerivedProperty#builder(AbtractWithDerivedProperty)} instead of private constructor to
+     * create new builder.
+     */
+    protected Builder( AbtractWithDerivedProperty pObject ) {
+    }
+  }
+
+  @Override
+  public int hashCode( ) {
+    return 1;
+  }
+
+  @Override
+  public boolean equals( Object pObject ) {
+    boolean lEquals;
+    if (this == pObject) {
+      lEquals = true;
+    }
+    else if (pObject == null) {
+      lEquals = false;
+    }
+    else if (this.getClass() != pObject.getClass()) {
+      lEquals = false;
+    }
+    else {
+      lEquals = true;
+    }
+    return lEquals;
+  }
+
+  /**
+   * Method returns a StringBuilder that can be used to create a String representation of this object. The returned
+   * StringBuilder also takes care about attributes of super classes.
+   *
+   * @return {@link StringBuilder} StringBuilder representing this object. The method never returns null.
+   */
+  public StringBuilder toStringBuilder( String pIndent ) {
+    StringBuilder lBuilder = new StringBuilder();
+    lBuilder.append(pIndent);
+    lBuilder.append(this.getClass().getName());
+    lBuilder.append(System.lineSeparator());
+    return lBuilder;
+  }
+
+  /**
+   * Method creates a new String with the values of all attributes of this class. All references to other objects will
+   * be ignored.
+   *
+   * @return {@link String} String representation of this object. The method never returns null.
+   */
+  @Override
+  public String toString( ) {
+    return this.toStringBuilder("").toString();
+  }
+}
