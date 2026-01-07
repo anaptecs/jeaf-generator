8d90c847153a056d94d54324b9257669b469316a
jeaf@anaptecs.de <114045600+jeaf-anaptecs@users.noreply.github.com>
2026-01-07 09:43:13 +0100

    Implemented and tested #492 (#662)

---
+++ jeaf-generator-test-jeaf-rest-proxy/src-gen/main/java/com/anaptecs/spring/base/AbtractWithDerivedProperty.java
@@ -0,0 +1,75 @@
+/*
+ * anaptecs GmbH, Ricarda-Huch-Str. 71, 72760 Reutlingen, Germany
+ *
+ * Copyright 2004 - 2019. All rights reserved.
+ */
+package com.anaptecs.spring.base;
+
+import java.io.Serializable;
+
+import com.anaptecs.jeaf.xfun.api.checks.Check;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+public abstract class AbtractWithDerivedProperty implements Serializable {
+  /**
+   * Default serial version UID.
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Initialize object using the passed builder.
+   *
+   * @param pBuilder Builder that should be used to initialize this object. The parameter must not be null.
+   */
+  protected AbtractWithDerivedProperty( Builder pBuilder ) {
+    // Ensure that builder is not null.
+    Check.checkInvalidParameterNull(pBuilder, "pBuilder");
+    // Read attribute values from builder.
+  }
+
+  /**
+   * Class implements builder to create a new instance of class <code>AbtractWithDerivedProperty</code>.
+   */
+  @JsonPOJOBuilder(withPrefix = "set")
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static abstract class Builder {
+    /**
+     * Use {@link AbtractWithDerivedProperty#builder()} instead of private constructor to create new builder.
+     */
+    protected Builder( ) {
+    }
+
+    /**
+     * Use {@link AbtractWithDerivedProperty#builder(AbtractWithDerivedProperty)} instead of private constructor to
+     * create new builder.
+     */
+    protected Builder( AbtractWithDerivedProperty pObject ) {
+    }
+  }
+
+  /**
+   * Method returns a StringBuilder that can be used to create a String representation of this object. The returned
+   * StringBuilder also takes care about attributes of super classes.
+   *
+   * @return {@link StringBuilder} StringBuilder representing this object. The method never returns null.
+   */
+  public StringBuilder toStringBuilder( String pIndent ) {
+    StringBuilder lBuilder = new StringBuilder();
+    lBuilder.append(pIndent);
+    lBuilder.append(this.getClass().getName());
+    lBuilder.append(System.lineSeparator());
+    return lBuilder;
+  }
+
+  /**
+   * Method creates a new String with the values of all attributes of this class. All references to other objects will
+   * be ignored.
+   *
+   * @return {@link String} String representation of this object. The method never returns null.
+   */
+  @Override
+  public String toString( ) {
+    return this.toStringBuilder("").toString();
+  }
+}
