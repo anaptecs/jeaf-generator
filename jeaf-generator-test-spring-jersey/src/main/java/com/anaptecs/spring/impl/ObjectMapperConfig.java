cfa04bfb15613b8a02c6a1a06f6762948879ee8d
JEAF Development Team <jeaf@anaptecs.de>
2026-01-13 11:59:28 +0100

    Added support for Jackson version-specific suffix

--- jeaf-generator-test-spring-jersey/src/main/java/com/anaptecs/spring/impl/ObjectMapperConfig.java
+++ jeaf-generator-test-spring-jersey/src/main/java/com/anaptecs/spring/impl/ObjectMapperConfig.java
@@ -1,16 +1,15 @@
 package com.anaptecs.spring.impl;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.zalando.problem.ProblemModule;
-
 import com.anaptecs.jeaf.rest.composite.api.CompositeTypeConverter;
 import com.anaptecs.jeaf.rest.composite.impl.kryo.KryoCompositeTypeConverter;
 import com.anaptecs.spring.base.serializers.ObjectMapperFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.zalando.problem.ProblemModule;
 
 /**
  * Jackson Object Mapper configuration that is supposed to be used in context of OSDM.
--- jeaf-generator-test-spring-rest-proxy/src/test/java/com/anaptecs/spring/test/ObjectMapperConfig.java
+++ jeaf-generator-test-spring-rest-proxy/src/test/java/com/anaptecs/spring/test/ObjectMapperConfig.java
@@ -1,14 +1,13 @@
 package com.anaptecs.spring.test;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.zalando.problem.ProblemModule;
-
 import com.anaptecs.spring.base.serializers.ObjectMapperFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.zalando.problem.ProblemModule;
 
 /**
  * Jackson Object Mapper configuration that is supposed to be used in context of OSDM.
