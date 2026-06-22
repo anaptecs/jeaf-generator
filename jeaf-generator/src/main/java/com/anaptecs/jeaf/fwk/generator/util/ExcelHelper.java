f7d7c6d97aa049cb35659ca430850e224dc438ef
JEAF Development Team <jeaf@anaptecs.de>
2026-06-22 11:17:40 +0200

    Updated excel file creation to reduce differences.

--- jeaf-generator/src/main/java/com/anaptecs/jeaf/fwk/generator/util/ExcelHelper.java
+++ jeaf-generator/src/main/java/com/anaptecs/jeaf/fwk/generator/util/ExcelHelper.java
@@ -4,14 +4,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.anaptecs.jeaf.tools.api.file.FileTools;
+import org.apache.poi.ooxml.POIXMLProperties;
+import org.apache.poi.ooxml.POIXMLProperties.CoreProperties;
+import org.apache.poi.ooxml.POIXMLProperties.ExtendedProperties;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelHelper {
   private static final List<List<String>> rowEntryCollector = new ArrayList<List<String>>();
@@ -26,8 +34,20 @@ public class ExcelHelper {
 
   public static void writeRowsToExcelTemplate(String pTemplateName, String pOutputFile) {
     try (InputStream lTemplateAsStream = FileTools.getFileTools().getResourceAsStream(pTemplateName)) {
-      Workbook lWorkbook = WorkbookFactory.create(lTemplateAsStream, null);
-      Sheet lSheet = lWorkbook.getSheetAt(0);
+      XSSFWorkbook lWorkbook = new XSSFWorkbook(lTemplateAsStream);
+      // Workbook lWorkbook = WorkbookFactory.create(lTemplateAsStream, null);
+      CoreProperties lProperties = lWorkbook.getProperties().getCoreProperties();
+      String FIXED_DATE = "2020-01-01T00:00:00Z";
+      lProperties.setCreator("JEAF Generator");
+      lProperties.setLastModifiedByUser("JEAF Generator");
+      lProperties.setCreated(FIXED_DATE);
+      lProperties.setModified(FIXED_DATE);
+
+      ExtendedProperties lExtendedProperties = lWorkbook.getProperties().getExtendedProperties();
+      lExtendedProperties.setApplication("JEAF Generator");
+      lExtendedProperties.setAppVersion("1.0");
+
+      XSSFSheet lSheet = lWorkbook.getSheetAt(0);
 
       int lRowIndex = 1;
       for (List<String> lNextRow : rowEntryCollector) {
@@ -44,7 +64,7 @@ public class ExcelHelper {
       lOutputStream.close();
       lWorkbook.close();
     }
-    catch (IOException e) {
+    catch (IOException | InvalidFormatException e) {
       e.printStackTrace();
       throw new RuntimeException("Unable to create Excel file ' " + pOutputFile + ".", e);
     }
