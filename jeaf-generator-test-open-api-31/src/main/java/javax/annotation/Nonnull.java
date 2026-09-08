3a4d6b9a6c460267d6be0f8b560257a53f7a8e7a
JEAF Development Team <jeaf@anaptecs.de>
2026-09-08 08:50:26 +0200

    Implemented and tested #768

---
+++ jeaf-generator-test-open-api-31/src/main/java/javax/annotation/Nonnull.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2004 - 2022
+ * anaptecs GmbH, Burgstr. 96, 72764 Reutlingen, Germany
+ *
+ * All rights reserved.
+ */
+package javax.annotation;
+
+public @interface Nonnull {
+
+}
